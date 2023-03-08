38bad498b31ea2afe516d352be9687c51e5e5426
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-03-08 17:48:19 -0400

    sd

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ require("dotenv").config();
 const app = require("./src/app.ts");
 const { PORT } = process.env;
 
-//
-
 app.listen(PORT, () => {
   console.log("🔥 > Server running on port", process.env.PORT);
 });
